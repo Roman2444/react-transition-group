73e22c20ba76a70237b531c4ee43f0544a63c57c
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2022-11-29 11:32:10 +0300

    addded CSSAnimation

--- src/App.js
+++ src/App.js
@@ -1,41 +1,23 @@
 import { useState, useRef } from 'react';
 import {Container} from 'react-bootstrap';
-import { Transition } from 'react-transition-group';
+import { CSSTransition } from 'react-transition-group';
 import './App.css';
 
 const Modal = (props) => {
 
     const duration = 300;
-
-    const defaultStyle = {
-    transition: `all ${duration}ms ease-in-out`,
-    opacity: 0,
-    visibility: "hidden"
-    }
-
-    const transitionStyles = {
-    entering: { opacity: 1, visibility: "visible" },
-    entered:  { opacity: 1, visibility: "visible"},
-    exiting:  { opacity: 0, visibility: "hidden" },
-    exited:  { opacity: 0, visibility: "hidden" },
-    };
-
-
    
-        const nodeRef = useRef(null);
+      
         return (
-          <Transition 
-              nodeRef={nodeRef} 
+          <CSSTransition 
               in={props.show} 
               timeout={duration} 
-              unmountOnExit
               onEnter={()=> props.setShowTrigger(false)}          
-              onExited={()=> props.setShowTrigger(true)}> 
-                {state => (
-                  <div ref={nodeRef} style={{
-                    ...defaultStyle,
-                    ...transitionStyles[state]
-                  }}>
+              onExited={()=> props.setShowTrigger(true)}
+              classNames="modal"
+              mountOnEnter
+              unmountOnExit> 
+                  
                     <div className="modal mt-5 d-block">
                         <div className="modal-dialog">
                             <div className="modal-content">
@@ -53,9 +35,8 @@ const Modal = (props) => {
                             </div>
                         </div>
                     </div>
-                  </div>
-                )}
-          </Transition>
+                  
+          </CSSTransition>
         );
       
 
